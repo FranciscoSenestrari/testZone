88befe24c2cf04a30ea6c281b64cd299268f99c2
FranciscoSenestrari <fran.senes@gmail.com>
2024-11-06 14:30:09 -0300

    contiuns qr fix

--- src/components/QR.tsx
+++ src/components/QR.tsx
@@ -5,15 +5,31 @@ export default function QR() {
     const [timestamp, setTimestamp] = useState(Date.now());
 
     // URL base sin parámetros específicos
-    const baseURL = " http://x.x.x.x:3000/destino";
-  
+    const baseURL = "http://192.168.100.49:3000/destino";
+   
     // Configura los parámetros, incluyendo el timestamp
     const params = new URLSearchParams({
       id: "12345",
       tipo: "temporal",
       timestamp: timestamp.toString(),
     });
-  
+function isTokenValid(tokenTimestamp:any) {
+  const currentTime = Date.now();
+  const timeElapsed = currentTime - tokenTimestamp;
+
+  // 1 minuto en milisegundos = 60,000 ms
+  const oneMinute = 60 * 1000;
+
+  return timeElapsed <= oneMinute;
+}
+
+// Ejemplo de uso
+const tokenTimestamp = Date.now(); // Marca el momento de creación del token
+setTimeout(() => {
+  console.log(isTokenValid(tokenTimestamp)); // Verifica si el token sigue siendo válido
+}, 500); // Cambia este valor
+    
+    console.log(btoa(params.toString()))
     // Genera la URL completa con los parámetros
     const qrURL = `${baseURL}?${params.toString()}`;
   
--- src/pages/ContinousQr.tsx
+++ src/pages/ContinousQr.tsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import { useLocation } from 'react-router';
-import Contenedor from '../components/Contenedor';
+import React from "react";
+import { useLocation } from "react-router";
+import Contenedor from "../components/Contenedor";
 import reactLogo from "../assets/react.svg";
 
 export default function ContinousQr() {
-    const location = useLocation();
-const params = new URLSearchParams(location.search);
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
   const id = params.get("id");
   const tipo = params.get("tipo");
-
-
+  const timestamp = params.get("timestamp");
+  console.log(timestamp);
   return (
-    <div className="">
-    <div className="flex justify-center items-center  ">
-      <a target="_blank" className="-ml-10">
-        <img src={reactLogo} className="logo react " alt="React logo" />
-      </a>
-      <div className="text-xl tiltle">Test Zone</div>
-    </div>
+    <div>
+      <div className="flex justify-center items-center  ">
+        <a target="_blank" className="-ml-10">
+          <img src={reactLogo} className="logo react " alt="React logo" />
+        </a>
+        <div className="text-xl tiltle">Test Zone</div>
+      </div>
 
-
-    <Contenedor>
+      <Contenedor>
         Contenido
         <div>
-      <h1>Página de Destino</h1>
-      <p>ID: {id}</p>
-      <p>Tipo: {tipo}</p>
+          <h1>Página de Destino</h1>
+          <p>ID: {id}</p>
+          <p>Tipo: {tipo}</p>
+        </div>
+      </Contenedor>
     </div>
-    </Contenedor>
-    </div>
-)
+  )
 }
